7a8a87d9b1d1359827303b9717a0ea367f5ad7d8
Doc Norton <michael@docondev.com>
2016-03-02 13:19:57 -0600

    introduce Density class

--- week-7/gps6/my_solution.rb
+++ week-7/gps6/my_solution.rb
@@ -9,9 +9,9 @@
 require_relative 'state_data'
 
 class VirusPredictor
-  def initialize(state_of_origin, density, count)
+  def initialize(state_of_origin, density_value, count)
     @state = state_of_origin
-    @population = Population.new(count, density)
+    @population = Population.new(count, density_value)
   end
 
   def virus_effects
@@ -20,62 +20,74 @@ class VirusPredictor
   end
 end
 
-class Population
-  EXTREME_DENSITY = 200
-  HIGH_DENSITY = 150
-  NORMAL_DENSITY = 100
-  LOW_DENSITY = 50
+class Density
+  EXTREME_BASE = 200
+  HIGH_BASE = 150
+  NORMAL_BASE = 100
+  LOW_BASE = 50
+  TINY_TOP = 49
 
-  def initialize(count, density)
-    @count = count
-    @density = density
+  def initialize(value)
+    @value = value
   end
 
-  def predicted_deaths
-    (@count * death_factor(@density)).floor
+  def is_extreme?
+    @value >= EXTREME_BASE
+  end
+
+  def is_high?
+    @value >= HIGH_BASE
+  end
+
+  def is_normal?
+    @value >= NORMAL_BASE
+  end
+
+  def is_low?
+    @value >= LOW_BASE
+  end
+
+  def is_tiny?
+    @value <= TINY_TOP
+  end
+
+  def death_factor
+      factor = 0.4 if is_extreme?
+      factor = 0.3 if is_high?
+      factor = 0.2 if is_normal?
+      factor = 0.1 if is_low?
+      factor = 0.05 if is_tiny?
+      factor
   end
 
   def months_to_spread
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    0.0 + speed_increment(@density)
-  end
 
-  private
-
-  def is_extreme_density?
-    @density >= EXTREME_DENSITY
-  end
-
-  def is_high_density?
-    @density >= HIGH_DENSITY
-  end
-
-  def is_normal_density?
-    @density >= NORMAL_DENSITY
-  end
-
-  def is_low_density?
-    @density >= LOW_DENSITY
-  end
-
-  def death_factor(population_density)
-      factor = 0.05
-      factor = 0.4 if is_extreme_density?
-      factor = 0.3 if is_high_density?
-      factor = 0.2 if is_normal_density?
-      factor = 0.1 if is_low_density?
-      factor
-  end
-
-  def speed_increment(population_density)
-    increment = 2.5
-    increment = 0.5 if is_extreme_density?
-    increment = 1 if is_high_density?
-    increment = 1.5 if is_normal_density?
-    increment = 2 if is_low_density?
+    increment = 0.5 if is_extreme?
+    increment = 1.0 if is_high?
+    increment = 1.5 if is_normal?
+    increment = 2.0 if is_low?
+    increment = 2.5 if is_tiny?
     increment
   end
+
+end
+
+class Population
+  def initialize(count, density_value)
+    @count = count
+    @density = Density.new(density_value)
+  end
+
+  def predicted_deaths
+    (@count * @density.death_factor).floor
+  end
+
+  def months_to_spread
+    @density.months_to_spread
+  end
+
 end
 
 #=======================================================================
