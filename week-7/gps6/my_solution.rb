580ccb2cc45270deebbd6092d2f964bc5813cbce
Doc Norton <michael@docondev.com>
2016-03-02 12:46:04 -0600

    created Population class

--- week-7/gps6/my_solution.rb
+++ week-7/gps6/my_solution.rb
@@ -9,59 +9,63 @@
 require_relative 'state_data'
 
 class VirusPredictor
-
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
-    @population = population
-    @population_density = population_density
+    @population = Population.new(population, population_density)
   end
 
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    print "#{@state} will lose #{@population.predicted_deaths} people in this outbreak"
+    puts " and will spread across the state in #{@population.speed_of_spread} months.\n\n"
+  end
+end
+
+class Population
+  def initialize(count, density)
+    @count = count
+    @density = density
+  end
+
+  def predicted_deaths
+    # predicted deaths is solely based on population density
+    (@count * death_factor(@density)).floor
+  end
+
+  def speed_of_spread #in months
+    # We are still perfecting our formula here. The speed is also affected
+    # by additional factors we haven't added into this functionality.
+    0.0 + speed_increment(@density)
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
-    # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+  def death_factor(population_density)
+    if population_density >= 200
+      0.4
+    elsif population_density >= 150
+      0.3
+    elsif population_density >= 100
+      0.2
+    elsif population_density >= 50
+      0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      0.05
     end
-
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-
   end
 
-  def speed_of_spread(population_density, state) #in months
-    # We are still perfecting our formula here. The speed is also affected
-    # by additional factors we haven't added into this functionality.
-    speed = 0.0
-
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
+  def speed_increment(population_density)
+    if population_density >= 200
+      0.5
+    elsif population_density >= 150
+      1
+    elsif population_density >= 100
+      1.5
+    elsif population_density >= 50
+      2
     else
-      speed += 2.5
+      2.5
     end
-
-    puts " and will spread across the state in #{speed} months.\n\n"
-
   end
-
 end
 
 #=======================================================================
